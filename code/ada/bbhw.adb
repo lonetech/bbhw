72e4ef637bcdd6e1067b9982211f3754e79a96dd
Yann Vernier <yann@vernier.se>
2024-07-20 19:41:48 +0200

    Simplified Ada version.

--- code/ada/bbhw.adb
+++ code/ada/bbhw.adb
@@ -1,46 +1,30 @@
--- Compile with: gnat make bbhw
-
-with Ada.Command_Line;    use Ada.Command_Line;
-with Ada.Strings;         use Ada.Strings;
-with Ada.Strings.Fixed;   use Ada.Strings.Fixed;
-with Ada.Text_IO;         use Ada.Text_IO;
+with Ada.Command_Line;  use Ada.Command_Line;
+with Ada.Strings;       use Ada.Strings;
+with Ada.Strings.Fixed; use Ada.Strings.Fixed;
+with Ada.Text_IO;       use Ada.Text_IO;
 
 procedure BBHW is	-- Bye Bye Hello World
    Count: Natural;
-   procedure Complain(S: String) is
+   function Check(S: String) return Boolean is
    begin
-      Put("Bad count " & S & ". ");
-   end;
-   procedure AskLoop is
-   begin
-      loop
-         Put("Count? ");
-         declare 
-            S: String := Get_Line;
-         begin
-            Count := Natural'Value(S);
-	    exit;
-         exception
-            when E : Constraint_Error =>
-	       Complain(S);
-         end;
-      end loop;
+      Count := Natural'Value(S);
+      return True;
+   exception
+      when E : Constraint_Error =>
+         Put("Bad count " & S & ". ");
+         return False;
    end;
 begin
-   if Argument_Count = 1 then
-      begin
-         Count := Natural'Value(Argument(1));
-      exception
-         when E : Constraint_Error =>
-	    Complain(Argument(1));
-	    AskLoop;
-      end;
-   else
-      AskLoop;
+   if Argument_Count /= 1
+	   or else not Check(Argument(1)) then
+      loop
+	 Put("Count? ");
+	 exit when Check(Get_Line);
+      end loop;
    end if;
    Put("World, Hello...");
    for I in reverse 1 .. Count loop
-      Put(Trim(Natural'Image(I), Left) & "...");
+      Put(Trim(I'Image, Left) & "...");
       delay Duration(1);
    end loop;
    Put_Line("Bye Bye.");
