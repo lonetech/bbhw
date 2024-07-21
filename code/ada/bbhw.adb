e9d0184b9ef6e2bd1ef62a9c30fd9c5962dc78c9
Yann Vernier <yann@vernier.se>
2024-07-21 08:49:21 +0200

    Style adjustments.
    Also adjusted messages to match other BBHW versions.

--- code/ada/bbhw.adb
+++ code/ada/bbhw.adb
@@ -3,29 +3,31 @@ with Ada.Strings;       use Ada.Strings;
 with Ada.Strings.Fixed; use Ada.Strings.Fixed;
 with Ada.Text_IO;       use Ada.Text_IO;
 
-procedure BBHW is	-- Bye Bye Hello World
-   Count: Natural;
-   function Check(S: String) return Boolean is
+procedure BBHW is  --  Bye Bye Hello World
+   Count : Natural;
+   function Check (S : String) return Boolean;
+   function Check (S : String) return Boolean is
    begin
-      Count := Natural'Value(S);
+      Count := Natural'Value (S);
       return True;
    exception
-      when E : Constraint_Error =>
-         Put("Bad count " & S & ". ");
+      when Unused : Constraint_Error =>
+         Put_Line ("Invalid countdown """ & S & """, try again...");
          return False;
-   end;
+   end Check;
 begin
    if Argument_Count /= 1
-	   or else not Check(Argument(1)) then
+     or else not Check (Argument (1))
+   then
       loop
-	 Put("Count? ");
-	 exit when Check(Get_Line);
+         Put ("countdown: ");
+         exit when Check (Get_Line);
       end loop;
    end if;
-   Put("World, Hello...");
-   for I in reverse 1 .. Count loop
-      Put(Trim(I'Image, Left) & "...");
-      delay Duration(1);
+   Put ("World, Hello...");
+   for J in reverse 1 .. Count loop
+      Put (Trim (J'Image, Left) & "...");
+      delay Duration (1);
    end loop;
-   Put_Line("Bye Bye.");
-end;
+   Put_Line ("Bye Bye.");
+end BBHW;
